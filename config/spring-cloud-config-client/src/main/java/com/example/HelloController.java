765f619e1d42d68bc91653cc930fc8c201c5371e
jingshuai <jin.gshuai@qq.com>
2018-07-06 04:11:47 +0800

    关于配置文件刷新

--- config/spring-cloud-config-client/src/main/java/com/example/HelloController.java
+++ config/spring-cloud-config-client/src/main/java/com/example/HelloController.java
@@ -1,9 +1,11 @@
 package com.example;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 class HelloController {
     @Value("${neo.hello}")
